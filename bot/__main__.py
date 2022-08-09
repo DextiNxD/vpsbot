d636e80497f70840ff66a686f9aa0da7c85a82f1
SpidyNaik69 <91309844+SpidyNaik69@users.noreply.github.com>
2022-08-09 21:49:15 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -61,10 +61,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/z_mirror")
+    buttons.buildbutton("Group☣️", "https://t.me/Spidymirror")
+    buttons.buildbutton("Owner😎", "https://t.me/Mr_Spidy")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
